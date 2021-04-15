cf30dbd21bd0d8d2f12cc471abb39e4ebddcaf9e
Mitchell Hynes <me@mitchellhynes.com>
2021-04-14 23:15:31 -0230

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod cli;
 mod parser;
 mod server;
 
@@ -7,13 +8,13 @@ use parser::*;
 use server::*;
 
 fn main() -> ParserResult<()> {
+    let matches = cli::matches();
+
     let source_list = discover_sources(Path::new("posts"))?;
     let read_sources = read_sources(source_list)?;
     let posts = parse_sources(read_sources)?;
-    // TODO: handle true/false via cli
-    generate_site(&posts, Some("public".into()), true)?;
-    // TODO: handle server via cli
-    // TODO: handle error in `ParserResult`
-    server();
+    let html_style = matches.is_present("html_slugs");
+    generate_site(&posts, Some("public".into()), !html_style)?;
+    server(matches.is_present("serve"), matches.is_present("console"))?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer};
 use actix_files::NamedFile;
-use actix_web::{Result};
+use actix_web::Result;
+use actix_web::{web, App, HttpRequest, HttpServer};
+use std::io;
 use std::path::PathBuf;
 
 async fn index(req: HttpRequest) -> Result<NamedFile> {
@@ -9,9 +10,16 @@ async fn index(req: HttpRequest) -> Result<NamedFile> {
 }
 
 #[actix_web::main]
-pub async fn server() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+pub async fn server(serve: bool, console: bool) -> io::Result<()> {
+    if serve || console {
+        HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
+            .bind("127.0.0.1:8080")?
+            .run()
+            .await;
+        let app = App::new().route("/{filename:.*}", web::get().to(index));
+        let server = HttpServer::new(|| app);
+        server.bind("127.0.0.1:8080")?.run().await
+    } else {
+        Ok(())
+    }
 }
