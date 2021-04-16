f8cb71d6447d83e470a219aff0c63f8e395cad11
Mitchell Hynes <me@mitchellhynes.com>
2021-04-16 16:24:30 -0230

    Bundling frontend

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,21 @@
-import _ from "lodash";
+import "./index.css";
+import logo from "./ploog.svg";
+import { h, render } from "preact";
+import { useState } from "preact/hooks";
 
-function component() {
-  const element = document.createElement("div");
+const App = () => {
+  const [state, setState] = useState(0);
+  return (
+    <div>
+      <div className="logo">
+        <span dangerouslySetInnerHTML={{ __html: logo }} />
+      </div>
+      <br />
+      <button onClick={() => setState((x) => x + 1)}>
+        Clicked {state} times{" "}
+      </button>
+    </div>
+  );
+};
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+render(<App />, document.body);
